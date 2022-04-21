671205de2cfec2ccb62c6c74032eaaf2aa747a6e
Magdiel <mgomez2091@gmail.com>
2022-04-21 09:25:36 -0600

    Change component Button

--- src/components/Buttons/Button.jsx
+++ src/components/Buttons/Button.jsx
@@ -1,15 +1,19 @@
+/* eslint-disable react/button-has-type */
 import React from 'react';
 
 import './button.scss';
 
-function Button({ children, styles, type, onClick, ...rest }) {
+function Button({ children, styles, ...rest }) {
   return (
     <button
-      className={styles === 'primary' ? 'btn btn-primary' : styles === 'secundary' ? 'btn btn-secundary' :
-        styles === 'secundary-white' ? 'btn btn-secundary-white' :
-          styles === 'ghost' ? 'btn btn-ghost' :
-            styles === 'ghost-variant' ? 'btn btn-ghost-variant' : 'btn-none'}
-      onClick={onClick}
+      className={
+        styles === 'primary' ? 'btn btn-primary' :
+          styles === 'secundary' ? 'btn btn-secundary' :
+            styles === 'secundary-white' ? 'btn btn-secundary-white' :
+              styles === 'ghost' ? 'btn btn-ghost' :
+                styles === 'ghost-variant' ?
+                  'btn btn-ghost-variant' : 'btn-none'
+      }
       {...rest}
     >
       {children}
