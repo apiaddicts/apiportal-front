658855dbba76d418f21a1df1bb8e3bd11433d47f
EnriqueTun23 <EnriqueTun23>
2022-04-25 12:52:06 -0500

    fix(blog): Added changes to be able to consume the blogs

--- src/components/FooterAuthor/index.jsx
+++ src/components/FooterAuthor/index.jsx
@@ -2,14 +2,15 @@ import React from 'react';
 import ItemAvatar from '../Item/ItemAvatar';
 import classes from './footer-social.module.scss';
 
-function FooterAuthor() {
+function FooterAuthor({ data }) {
+  console.log(data);
   return (
     <div className={classes.footer__author}>
       <ItemAvatar
         divider
-        img='https://api.lorem.space/image/face?w=150&h=150'
-        title='María Mercedes Barrera'
-        paragraph='Directora Ejecutiva Fundación Sura'
+        img={data.photoUser && data.photoUser.length > 0 ? data.photoUser[0].url : 'https://api.lorem.space/image/face?w=150&h=150'}
+        title={data.nameUser ? data.nameUser : ''}
+        paragraph={data.jobUser ? data.jobUser : ''}
       />
     </div>
   );
