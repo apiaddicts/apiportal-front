c7ed66e0e7132953328d0fc8565a693f61b5129a
Magdiel <mgomez2091@gmail.com>
2022-05-04 16:34:45 -0700

    Add icons

--- src/components/common/InputMUI/index.jsx
+++ src/components/common/InputMUI/index.jsx
@@ -1,25 +1,28 @@
 import React from 'react';
-import Input from './style';
+import Input, { ContainerIcon, ContainerInput } from './style';
 
-function TextField({ field, formik, ...rest }) {
+function TextField({ field, formik, iconEye, iconCopy }) {
   const { values, errors, touched, handleChange, handleBlur, isSelect } = formik;
 
   return (
-    <Input
-      select={isSelect}
-      fullWidth
-      required={field.required && true}
-      id={field.id}
-      label={field.label}
-      disabled={field.disabled}
-      error={touched[field.id] && !!errors[field.id]}
-      type={field.type}
-      onChange={handleChange}
-      onBlur={handleBlur}
-      placeholder={field.placeholder}
-      value={values[field.id]}
-      // helperText={(touched[field.id] && errors[field.id]) ?? null}
-    />
+    <ContainerInput>
+      <ContainerIcon icon={true} />
+      <Input
+        select={isSelect}
+        fullWidth
+        required={field.required && true}
+        id={field.id}
+        label={field.label}
+        disabled={field.disabled}
+        error={touched[field.id] && !!errors[field.id]}
+        type={field.type}
+        onChange={handleChange}
+        onBlur={handleBlur}
+        placeholder={field.placeholder}
+        value={values[field.id]}
+        // helperText={(touched[field.id] && errors[field.id]) ?? null}
+      />
+    </ContainerInput>
   );
 };
 
