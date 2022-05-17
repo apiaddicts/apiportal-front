9a9400a2406221ca8f406bfec627e0d1bea7fae4
Mikel Landa <mikel.landa@cloudappi.net>
2022-05-17 16:53:52 +0200

    pipeline test

--- src/services/config.js
+++ src/services/config.js
@@ -1,11 +1,12 @@
 const config = {
-//   apiUrl: 'http://localhost:1340',
-  apiUrl: 'https://pre-strapi-sura.cloudappi.net',
-  suraUrl: 'https://cloudappi-apim.management.azure-api.net',
-  subscriptionId: '8e7674e5-98c5-4ccb-9b4c-bd5eec3dc202',
-  resourceGroupName: 'grdatafactory',
-  serviceName: 'cloudappi-apim',
-  apiVersion: '2021-08-01',
-  hmacAuthHeader: 'SharedAccessSignature integration&202206090144&mezCbWwQlNcohcjSez0cmEmhTBjc4PBgR7DGSSK5DbA1VN8m6//JxgaGZ+hSJaDFaM7u8hWk9BUS8MVpKRJ72g==',
+	//   apiUrl: 'http://localhost:1340',
+	apiUrl: process.env.STRAPI_URL,
+	suraUrl: process.env.SURA_URL,
+	subscriptionId: process.env.SUBSCRIPTION_ID,
+	resourceGroupName: process.env.RESOURCE_GROUP_NAME,
+	serviceName: process.env.SERVICE_NAME,
+	apiVersion: "2021-08-01",
+	hmacAuthHeader:
+		"SharedAccessSignature integration&202206090144&mezCbWwQlNcohcjSez0cmEmhTBjc4PBgR7DGSSK5DbA1VN8m6//JxgaGZ+hSJaDFaM7u8hWk9BUS8MVpKRJ72g=="
 };
 export default config;
