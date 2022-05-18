720e353b0acbb5de753ca2caac0f26f27e15e577
EnriqueTun23 <EnriqueTun23>
2022-05-18 10:45:29 -0500

    feat(environment variables): The new environment variables were saved and the adjustment was made so as not to modify this.

--- src/services/config.js
+++ src/services/config.js
@@ -1,12 +1,11 @@
 const config = {
-	//   apiUrl: 'http://localhost:1340',
-	apiUrl: process.env.STRAPI_URL,
-	suraUrl: process.env.SURA_URL,
-	subscriptionId: process.env.SUBSCRIPTION_ID,
-	resourceGroupName: process.env.RESOURCE_GROUP_NAME,
-	serviceName: process.env.SERVICE_NAME,
-	apiVersion: "2021-08-01",
-	hmacAuthHeader:
-		"SharedAccessSignature integration&202206090144&mezCbWwQlNcohcjSez0cmEmhTBjc4PBgR7DGSSK5DbA1VN8m6//JxgaGZ+hSJaDFaM7u8hWk9BUS8MVpKRJ72g=="
+  //   apiUrl: 'http://localhost:1340',
+  apiUrl: process.env.REACT_APP_STRAPI_URL,
+  suraUrl: process.env.SURA_URL,
+  subscriptionId: process.env.SUBSCRIPTION_ID,
+  resourceGroupName: process.env.RESOURCE_GROUP_NAME,
+  serviceName: process.env.SERVICE_NAME,
+  apiVersion: '2021-08-01',
+  hmacAuthHeader: 'SharedAccessSignature integration&202206090144&mezCbWwQlNcohcjSez0cmEmhTBjc4PBgR7DGSSK5DbA1VN8m6//JxgaGZ+hSJaDFaM7u8hWk9BUS8MVpKRJ72g==',
 };
 export default config;
