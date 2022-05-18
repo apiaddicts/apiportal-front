75eb7cd4d93d5015b2dad5830cb600e4f33262cd
Juan Enrique Rojas Sanchez <juan.rojas.pe@cloudappi.net>
2022-05-18 16:40:25 -0500

    Test environment var

--- src/services/config.js
+++ src/services/config.js
@@ -6,6 +6,6 @@ const config = {
   resourceGroupName: process.env.REACT_APP_RESOURCE_GROUP_NAME,
   serviceName: process.env.REACT_APP_SERVICE_NAME,
   apiVersion: "2021-08-01",
-  hmacAuthHeader: "SharedAccessSignature integration&202206090144&mezCbWwQlNcohcjSez0cmEmhTBjc4PBgR7DGSSK5DbA1VN8m6//JxgaGZ+hSJaDFaM7u8hWk9BUS8MVpKRJ72g==",
+  hmacAuthHeader: "SharedAccessSignature integration&202206090144&mezCbWwQlNcohcjSez0cmEmhTBjc4PBgR7DGSSK5DbA1VN8m6//JxgaGZ+hSJaDFaM7u8hWk9BUS8MVpKRJ72g=="
 };
 export default config;
