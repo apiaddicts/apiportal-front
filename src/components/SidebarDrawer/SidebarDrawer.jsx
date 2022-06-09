5c0050966ce5cb810260c30d21b0385c0189f870
Jesus Rafael Nava Cruz <jesus.nava.mx@cloudappi.net>
2022-06-09 14:48:01 -0500

    FEAT: Create Drawer temporary from small devices

--- src/components/SidebarDrawer/SidebarDrawer.jsx
+++ src/components/SidebarDrawer/SidebarDrawer.jsx
@@ -1,28 +1,55 @@
 /* eslint-disable no-use-before-define */
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 import { useDispatch } from 'react-redux';
 
 import { NavLink, useNavigate } from 'react-router-dom';
+import { styled, useTheme } from '@mui/material/styles';
 
-import Drawer from '@mui/material/Drawer';
+import MuiDrawer from '@mui/material/Drawer';
 import List from '@mui/material/List';
 import ListItem, { listItemClasses } from '@mui/material/ListItem';
 import ListItemText from '@mui/material/ListItemText';
 import ListItemIcon from '@mui/material/ListItemIcon';
 import Terminal from '@mui/icons-material/Terminal';
 import Settings from '@mui/icons-material/Settings';
-import { AppBar, Box, Button, Menu, MenuItem, Toolbar } from '@mui/material';
+import { AppBar, Box, Button, IconButton, Menu, MenuItem, Toolbar } from '@mui/material';
 import { ChevronLeft } from '@mui/icons-material';
 import PersonSharpIcon from '@mui/icons-material/PersonSharp';
 import KeyboardArrowDownSharpIcon from '@mui/icons-material/KeyboardArrowDownSharp';
 import LogoutIcon from '@mui/icons-material/Logout';
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
 import classes from './sliderdrawer.module.scss';
 import SuraLogoAlt from '../../static/img/sura_logo_alt.svg';
-
 import { logout } from '../../redux/actions/userAction';
+// import { width } from '@mui/system';
 
 function SidebarDrawer({ children, user }) {
+  const theme = useTheme();
+  const [isPermanent, setPermanent] = useState(true);
+  const [toggleMenu, setToggleMenu] = useState(false);
+
+  function handleWindowWithChange() {
+    const windowWidth = window.innerWidth;
+    const breakpoint = theme.breakpoints.values.md;
+    const isSm = windowWidth < breakpoint;
+
+    if (isSm && isPermanent) {
+      setPermanent(false);
+    } else if (!isSm && !isPermanent) {
+      setPermanent(true);
+    }
+  }
+
+  useEffect(() => {
+    window.addEventListener('resize', handleWindowWithChange);
+    handleWindowWithChange();
+    return () => {
+      window.removeEventListener('resize', handleWindowWithChange());
+    };
+  });
+
   const listItems = [
     { route: '/products', text: 'Productos', icon: <Terminal /> },
     { route: '/apiBookstores', text: 'Biblioteca de APIs', icon: <Settings /> },
@@ -49,6 +76,59 @@ function SidebarDrawer({ children, user }) {
     dispatch(logout());
   };
 
+  const handleMenu = () => {
+    setToggleMenu(!toggleMenu);
+  };
+
+  const drawerWidth = 300;
+  const openMenu = (theme) => ({
+    width: drawerWidth,
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.enteringScreen,
+    }),
+    overflowX: 'hidden',
+  });
+
+  const closeMenu = (theme) => ({
+    transition: theme.transitions.create('width', {
+      easing: theme.transitions.easing.sharp,
+      duration: theme.transitions.duration.leavingScreen,
+    }),
+    overflowX: 'hidden',
+    width: '146px',
+    [theme.breakpoints.up('sm')]: {
+      width: '146px',
+    },
+  });
+
+  const Drawer = styled(MuiDrawer, { shouldForwardProp: (prop) => prop !== 'open' })(
+    ({ theme, open }) => ({
+      width: drawerWidth,
+      flexShrink: 0,
+      whithSpace: 'nowrap',
+      boxSizing: 'border-box',
+      ...(open && {
+        ...openMenu(theme),
+        '& .MuiDrawer-paper': openMenu(theme),
+      }),
+      ...(!open && {
+        ...closeMenu(theme),
+        '& .MuiDrawer-paper': closeMenu(theme),
+      }),
+    }),
+  );
+  const DrawerHeader = styled('div')(({ theme }) => ({
+    width: '100%',
+    background: '#0033A0',
+    display: 'flex',
+    alignItems: 'center',
+    padding: theme.spacing(1, 3),
+    // necessary for content to be below app bar
+    ...theme.mixins.toolbar,
+    justifyContent: 'space-between',
+  }));
+
   return (
     <div className={classes.backgroundSidebar}>
       {/* <CssBaseline /> */}
@@ -56,77 +136,93 @@ function SidebarDrawer({ children, user }) {
         <AppBar position='fixed' elevation={0} sx={{ zIndex: (theme) => theme.zIndex.drawer + 1, background: '#0033a0', padding: '0 100px' }}>
           <Toolbar disableGutters sx={{ display: 'flex', justifyContent: 'space-between', alignItems: 'center' }}>
             <img src={SuraLogoAlt} alt='Sura Logo' />
-            <Box sx={{ display: 'flex', alignItems: 'center' }}>
-              <Button
-                variant='outlined'
-                startIcon={<PersonSharpIcon sx={{ color: '#00AEC7' }} />}
-                endIcon={<KeyboardArrowDownSharpIcon color='white' sx={{ color: '#fff' }} />}
-                sx={{
-                  borderRadius: '20px',
-                  border: '1px solid #00AEC7',
-                  color: '#fff',
-                  marginRight: '1rem',
-                }}
-                onClick={handleClick}
-              >
-                {user && Object.keys(user).length > 0 && user.properties && Object.keys(user.properties).length > 0 ? (
-                  user.properties.firstName
-                ) : ('')}
-              </Button>
-              <Menu
-                id='basic-menu'
-                anchorEl={anchorEl}
-                open={open}
-                onClose={handleClose}
-                MenuListProps={{
-                  'aria-labelledby': 'demo-customized-button',
-                }}
-                anchorOrigin={{
-                  vertical: 'bottom',
-                  horizontal: 'right',
-                }}
-                transformOrigin={{
-                  vertical: 'top',
-                  horizontal: 'right',
-                }}
-              >
-                <MenuItem onClick={handleUser}>
-                  <ListItemIcon>
-                    <PersonSharpIcon />
-                  </ListItemIcon>
-                  <ListItemText>
-                    Mi perfil
-                  </ListItemText>
-                </MenuItem>
-                <MenuItem onClick={handleLogout}>
-                  <ListItemIcon>
-                    <LogoutIcon />
-                  </ListItemIcon>
-                  <ListItemText>
-                    Salir
-                  </ListItemText>
-                </MenuItem>
-              </Menu>
-            </Box>
+            {isPermanent && (
+              <Box sx={{ display: 'flex', alignItems: 'center' }}>
+                <Button
+                  variant='outlined'
+                  startIcon={<PersonSharpIcon sx={{ color: '#00AEC7' }} />}
+                  endIcon={<KeyboardArrowDownSharpIcon color='white' sx={{ color: '#fff' }} />}
+                  sx={{
+                    borderRadius: '20px',
+                    border: '1px solid #00AEC7',
+                    color: '#fff',
+                    marginRight: '1rem',
+                  }}
+                  onClick={handleClick}
+                >
+                  {user && Object.keys(user).length > 0 && user.properties && Object.keys(user.properties).length > 0 ? (
+                    user.properties.firstName
+                  ) : ('')}
+                </Button>
+                <Menu
+                  id='basic-menu'
+                  anchorEl={anchorEl}
+                  open={open}
+                  onClose={handleClose}
+                  MenuListProps={{
+                    'aria-labelledby': 'demo-customized-button',
+                  }}
+                  anchorOrigin={{
+                    vertical: 'bottom',
+                    horizontal: 'right',
+                  }}
+                  transformOrigin={{
+                    vertical: 'top',
+                    horizontal: 'right',
+                  }}
+                >
+                  <MenuItem onClick={handleUser}>
+                    <ListItemIcon>
+                      <PersonSharpIcon />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Mi perfil
+                    </ListItemText>
+                  </MenuItem>
+                  <MenuItem onClick={handleLogout}>
+                    <ListItemIcon>
+                      <LogoutIcon />
+                    </ListItemIcon>
+                    <ListItemText>
+                      Salir
+                    </ListItemText>
+                  </MenuItem>
+                </Menu>
+              </Box>
+            )}
+            {
+              !isPermanent ? (
+                <IconButton
+                  size='large'
+                  edge='start'
+                  color='inherit'
+                  aria-label='menu'
+                  sx={{ mr: 2 }}
+                  onClick={handleMenu}
+                >
+                  <MenuIcon />
+                </IconButton>
+              ) : null
+            }
           </Toolbar>
         </AppBar>
       </Box>
       <Drawer
-        variant='permanent'
-        sx={{
-          width: '300px',
-        }}
-
+        variant='temporary'
+        open={toggleMenu}
+        onClose={handleMenu}
+        anchor='right'
       >
-        <List
-          sx={{ paddingRight: '80px' }}
-        >
-          <ListItem
-            sx={{
-              paddingLeft: '97px',
-              paddingTop: '80px',
-            }}
-          >
+        <DrawerHeader>
+          <div>
+            <img src={SuraLogoAlt} alt='' />
+          </div>
+          <div className='text__secondary'>
+            <CloseIcon />
+          </div>
+        </DrawerHeader>
+        <List>
+          <ListItem>
             <ListItemText>
               <h1 className={`font-weight-regular text__gray__gray_darken ${classes.title}`}>Hola,</h1>
               <h1 className={`font-weight-bold text__primary ${classes.title__name}`}>
@@ -142,23 +238,114 @@ function SidebarDrawer({ children, user }) {
               </h1>
             </ListItemText>
           </ListItem>
-        </List>
-        <List>
-          <ListItem
-            sx={{
-              padding: '25px 80px 0px 97px',
-            }}
-          >
-            {/* <ListItemText>
-              <Typography variant='h6' component='div'>
-                <p className={classes.title__organization}>Organización</p>
-              </Typography>
-              <Typography variant='h6' component='div'>
-                <p className={classes.title__role}>Administrador</p>
-              </Typography>
-            </ListItemText> */}
+          <ListItem>
+            <ListItemText>
+              <h1 className={classes.title__developer}>developer portal</h1>
+            </ListItemText>
           </ListItem>
         </List>
+        <List
+          sx={{
+            [`& .active, & .${listItemClasses.root}:hover`]: {
+              color: '#0033a0',
+              fontWeight: '700',
+              background: 'rgb(0, 174, 199, 0.1)',
+              width: '100%',
+              '& svg': {
+                fill: '#0033a0',
+              },
+            },
+          }}
+        >
+          {
+            listItems.map((item, index) => (
+              <ListItem button key={index} sx={{ color: '#53565A' }} component={MyNavLink} to={item.route}>
+                <ListItemIcon>
+                  {item.icon}
+                </ListItemIcon>
+                <ListItemText primary={item.text} />
+              </ListItem>
+            ))
+          }
+        </List>
+        <List>
+          <ListItem>
+            <div style={{ borderBottom: '1px solid #E3E829', width: '58px' }} />
+          </ListItem>
+          <ListItem
+            button
+            sx={{ color: '#00AEC7', fontWeight: '500' }}
+            onClick={handleUser}
+          >
+            <ListItemIcon>
+              <svg width='18' height='18' viewBox='0 0 18 18' fill='none' xmlns='http://www.w3.org/2000/svg'>
+                <path fillRule='evenodd' clipRule='evenodd' d='M9.44755 0C11.516 0 13.2098 1.5889 13.2098 3.54227C13.2098 5.49565 11.516 7.08455 9.44755 7.08455C7.37915 7.08455 5.68531 5.49565 5.68531 3.54227C5.68531 1.5889 7.37915 0 9.44755 0ZM15 18V7.55685H5.92882C4.31425 7.55685 3 8.73995 3 10.2134V18H15ZM5.92882 8.78433H13.6979V16.8032H4.30205V10.2288L4.30616 10.1272C4.36611 9.38822 5.07411 8.78433 5.92882 8.78433ZM6.96869 3.54227C6.96869 2.27019 8.07275 1.23011 9.44755 1.23011C10.8224 1.23011 11.9264 2.27019 11.9264 3.54227C11.9264 4.80884 10.8118 5.85444 9.44755 5.85444C8.07275 5.85444 6.96869 4.81436 6.96869 3.54227Z' fill='white' />
+                <mask id='mask0_2049_231200' maskUnits='userSpaceOnUse' x='3' y='0' width='12' height='18'>
+                  <path fillRule='evenodd' clipRule='evenodd' d='M9.44755 0C11.516 0 13.2098 1.5889 13.2098 3.54227C13.2098 5.49565 11.516 7.08455 9.44755 7.08455C7.37915 7.08455 5.68531 5.49565 5.68531 3.54227C5.68531 1.5889 7.37915 0 9.44755 0ZM15 18V7.55685H5.92882C4.31425 7.55685 3 8.73995 3 10.2134V18H15ZM5.92882 8.78433H13.6979V16.8032H4.30205V10.2288L4.30616 10.1272C4.36611 9.38822 5.07411 8.78433 5.92882 8.78433ZM6.96869 3.54227C6.96869 2.27019 8.07275 1.23011 9.44755 1.23011C10.8224 1.23011 11.9264 2.27019 11.9264 3.54227C11.9264 4.80884 10.8118 5.85444 9.44755 5.85444C8.07275 5.85444 6.96869 4.81436 6.96869 3.54227Z' fill='white' />
+                </mask>
+                <g mask='url(#mask0_2049_231200)'>
+                  <circle cx='9' cy='9' r='9' fill='#00AEC7' />
+                  <rect width='18' height='18' fill='#00AEC7' />
+                </g>
+              </svg>
+
+            </ListItemIcon>
+            <ListItemText>
+              Mi Perfil
+            </ListItemText>
+          </ListItem>
+        </List>
+        <List>
+          <ListItem button onClick={handleLogout}>
+            <ListItemIcon>
+              <LogoutIcon />
+            </ListItemIcon>
+            <ListItemText>Salir</ListItemText>
+          </ListItem>
+        </List>
+      </Drawer>
+      <Drawer
+        variant='permanent'
+        sx={{
+          display: { xs: 'none', md: 'block' },
+        }}
+        open={toggleMenu}
+      >
+        {toggleMenu && (
+          <List
+            sx={{ paddingRight: '80px' }}
+          >
+            <ListItem
+              sx={{
+                paddingLeft: '97px',
+                paddingTop: '80px',
+              }}
+            >
+              <ListItemText>
+                <h1 className={`font-weight-regular text__gray__gray_darken ${classes.title}`}>Hola,</h1>
+                <h1 className={`font-weight-bold text__primary ${classes.title__name}`}>
+                  {
+                    user && Object.keys(user).length > 0 && user.properties && Object.keys(user.properties).length > 0 ? (
+                      <>
+                        {user.properties.firstName}
+                        <br />
+                        {user.properties.lastName }
+                      </>
+                    ) : ('')
+                  }
+                </h1>
+              </ListItemText>
+            </ListItem>
+            <ListItem
+              sx={{ paddingLeft: '97px' }}
+            >
+              <ListItemText>
+                <h1 className={classes.title__developer}>developer portal</h1>
+              </ListItemText>
+            </ListItem>
+          </List>
+        )}
+
         <List
           sx={{
             [`& .active, & .${listItemClasses.root}:hover`]: {
@@ -172,16 +359,9 @@ function SidebarDrawer({ children, user }) {
             },
           }}
         >
-          <ListItem
-            sx={{ paddingLeft: '97px' }}
-          >
-            <ListItemText>
-              <h1 className={classes.title__developer}>developer portal</h1>
-            </ListItemText>
-          </ListItem>
           {
             listItems.map((item, index) => (
-              <ListItem button key={index} sx={{ color: '#53565A', paddingLeft: '97px' }} component={MyNavLink} to={item.route} exact>
+              <ListItem button key={index} sx={{ color: '#53565A', paddingLeft: '97px', paddingRight: 0 }} component={MyNavLink} to={item.route} exact>
                 <ListItemIcon>
                   {item.icon}
                 </ListItemIcon>
@@ -193,9 +373,8 @@ function SidebarDrawer({ children, user }) {
         <List>
           <ListItem
             button
-            sx={{
-              paddingLeft: '97px',
-            }}
+            onClick={handleMenu}
+            sx={{ paddingLeft: '97px' }}
           >
             <ListItemIcon>
               <ChevronLeft />
