be0187f9666ea386b11d243bed30ffe8a334e888
Francisco Flores <paco.flores.mx@cloudappi.net>
2022-07-02 22:09:05 -0500

    Add env vars

--- src/services/config.js
+++ src/services/config.js
@@ -34,6 +34,7 @@ const config = {
   homePageSlug: process.env.REACT_APP_HOME_PAGE_SLUG,
   blogPageSlug: process.env.REACT_APP_BLOG_PAGE_SLUG,
   faqPageSlug: process.env.REACT_APP_FAQ_PAGE_SLUG,
+  apisPageSlug: process.env.REACT_APP_APIS_PAGE_SLUG,
   locale: 'es',
 };
 
