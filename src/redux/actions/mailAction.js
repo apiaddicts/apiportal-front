ecd825d834423f0486ce4f67b4e75b1fe52181ac
Uziel Trujillo <uziel.trujillo.mx@cloudappi.net>
2022-07-22 11:46:40 -0500

    Implement ContactMail 	modified:   src/components/Contact/index.jsx 	modified:   src/components/Footer/Footer.jsx 	modified:   src/pages/public/Blog/index.jsx 	modified:   src/redux/actions/mailAction.js 	modified:   src/redux/constants/mailConstants.js 	modified:   src/redux/reducer/mailReducer.js 	modified:   src/services/config.js 	modified:   src/services/mailService.js

--- src/redux/actions/mailAction.js
+++ src/redux/actions/mailAction.js
@@ -7,19 +7,43 @@ export const sendContactMail = (values) => (dispatch) => {
     (response) => {
       if (response.ok) {
         dispatch({
-          type: mailConstants.GET_ALL_MAIL_SUCCESS,
+          type: mailConstants.GET_ALL_MAIL_CONTACT_SUCCESS,
           payload: response,
         });
       } else {
         dispatch({
-          type: mailConstants.GET_ALL_MAIL_FAILURE,
+          type: mailConstants.GET_ALL_MAIL_CONTACT_FAILURE,
           payload: response,
         });
       }
     },
     (error) => {
       dispatch({
-        type: mailConstants.GET_ALL_MAIL_FAILURE,
+        type: mailConstants.GET_ALL_MAIL_CONTACT_FAILURE,
+        payload: error.response,
+      });
+    },
+  );
+};
+
+export const sendConversationMail = (values) => (dispatch) => {
+  mailService.sendConversationMail(values).then(
+    (response) => {
+      if (response.ok) {
+        dispatch({
+          type: mailConstants.GET_ALL_MAIL_CONVERSATION_SUCCESS,
+          payload: response,
+        });
+      } else {
+        dispatch({
+          type: mailConstants.GET_ALL_MAIL_CONVERSATION_FAILURE,
+          payload: response,
+        });
+      }
+    },
+    (error) => {
+      dispatch({
+        type: mailConstants.GET_ALL_MAIL_CONVERSATION_FAILURE,
         payload: error.response,
       });
     },
