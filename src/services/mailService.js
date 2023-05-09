fc799237b8dfb05d2c358407e3712507c3cdcfd3
David Naar <naardavid.t@gmail.com>
2023-05-08 20:36:29 -0500

    fix contact mail

--- src/services/mailService.js
+++ src/services/mailService.js
@@ -1,51 +1,18 @@
 import config from './config';
 
-function sendContactMail(values) {
-  let htmlContent = '<ul>';
-  Object.keys(values).forEach((key) => {
-    htmlContent += `<li>${key}: ${values[key]}</li>`;
-  });
-  htmlContent += '</ul>';
+const sendContactMail = (values) => {
   const data = {
     'to': config.emailTo,
     'from': config.emailFrom,
+    'subject': values['subject'],
     'templateId': config.emailContactTemplateId,
-    'dynamicTemplateData': {
-      'SUBJECT': 'Sigamos conectados',
-      'CONTENIDO': htmlContent,
-    },
-  };
-  const requestOptions = {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-      'apiKeySendGrid': config.apiKeySendGrid,
-      'Subscription-Key': config.subscriptionKey,
-    },
-    body: JSON.stringify(data),
-  };
-  return fetch(config.emailUrl, requestOptions)
-    .then((response) => {
-      return response;
-    }).catch((error) => {
-      console.error(error);
-      return error.status;
-    });
-}
-
-function sendConversationMail(values) {
-  let htmlContent = '<ul>';
-  Object.keys(values).forEach((key) => {
-    htmlContent += `<li>${key}: ${values[key]}</li>`;
-  });
-  htmlContent += '</ul>';
-  const data = {
-    'to': config.emailTo,
-    'from': config.emailFrom,
-    'templateId': config.emailConversationTemplateId,
-    'dynamicTemplateData': {
-      'SUBJECT': '¿Conversamos?',
-      'CONTENIDO': htmlContent,
+    'emailData': {
+      'firstName': values['name'],
+      'lastName': values['lastname'],
+      'email': values['email'],
+      'phone': values['phone'],
+      'topic': values['subject'],
+      'message': values['message'],
     },
   };
 
@@ -53,8 +20,6 @@ function sendConversationMail(values) {
     method: 'POST',
     headers: {
       'Content-Type': 'application/json',
-      'apiKeySendGrid': config.apiKeySendGrid,
-      'Subscription-Key': config.subscriptionKey,
     },
     body: JSON.stringify(data),
   };
@@ -63,14 +28,12 @@ function sendConversationMail(values) {
     .then((response) => {
       return response;
     }).catch((error) => {
-      console.error(error);
       return error.status;
     });
-}
-
-const mailService = {
-  sendContactMail,
-  sendConversationMail,
 };
 
-export default mailService;
+const emailService = {
+  sendContactMail,
+};
+
+export default emailService;
