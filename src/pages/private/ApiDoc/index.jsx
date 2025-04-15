1b147852b1349e0df089cc8128bb9bcef4125061
jhonatan Ibarra <jhonatan.ibarra.co@cloudappi.net>
2025-04-15 16:02:47 -0500

    Actualizar componente ApiDoc: ajustar diseño de Grid y mejorar la disposición de elementos

--- src/pages/private/ApiDoc/index.jsx
+++ src/pages/private/ApiDoc/index.jsx
@@ -15,10 +15,11 @@ function ApiDoc() {
         Lorem Ipsum es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen.
       </Typography>
 
-      <Grid container spacing={3} sx={{ marginTop: '20px' }} justifyContent="space-between">
+      <Grid container spacing={3} sx={{ marginTop: '20px' }} justifyContent="space-between" wrap="no-wrap">
+
 
         {/* File Upload Section */}
-        <Grid item xs={12} md={6}>
+        <Grid item xs={12} md={4}>
           <Card>
             <CardContent>
               <Typography variant="subtitle1" gutterBottom>
