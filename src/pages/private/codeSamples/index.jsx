b9f4dc595652c4c601c822761aadeb2d683a842c
jhonatan Ibarra <jhonatan.ibarra.co@cloudappi.net>
2025-04-25 08:56:24 -0500

    feat: update CodeSamples component layout and improve text clarity

--- src/pages/private/codeSamples/index.jsx
+++ src/pages/private/codeSamples/index.jsx
@@ -19,23 +19,27 @@ function CodeSamples(props) {
 
   return (
 
-    <Container fixed sx={{ paddingLeft: { xs: '0px', md: '59px !important' }, paddingRight: { xs: '0px', md: '97px !important' } }}>
-      <Box sx={{ maxWidth: 1000, mx: 'auto', px: 2, py: 6, textAlign: 'center' }}>
+    <Container maxWidth={false} sx={{ maxWidth: '1200px', mx: 'auto', px: 2 }}>
+      <Box sx={{ mx: 'auto', py: 6, textAlign: 'center' }}>
         <Typography variant="h4" fontWeight={600} mb={2}>
           Code samples
         </Typography>
         <Typography color="text.secondary" mb={5}>
-          Luxand.cloud API contains a wide range of ready-to-use cases. Choose the relevant case
-          and access the comprehensive documentation provided.
+          API contains a wide range of ready-to-use cases...
         </Typography>
 
-        <Grid container spacing={4} justifyContent="center">
+        <Grid container spacing={4}>
           {apis.map((apiItem, index) => (
-            <Grid item xs={12} sm={6} md={3} key={index}>
+            <Grid item size={4} key={index}>
               <Link to={`/developer/code-samples/${apiItem.assetId}`} underline="none" color="inherit">
                 <Paper
                   elevation={0}
                   sx={{
+                    height: '150px',
+                    display: 'flex',
+                    flexDirection: 'column',
+                    justifyContent: 'center',
+                    alignItems: 'center',
                     p: 3,
                     textAlign: 'center',
                     border: '1px solid #e0e0e0',
@@ -62,6 +66,7 @@ function CodeSamples(props) {
 
       </Box>
     </Container>
+
   );
 }
 
