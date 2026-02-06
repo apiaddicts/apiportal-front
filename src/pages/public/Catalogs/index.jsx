457ffeb3d8d0c0b58b9d03c72233377953bef289
leonardoherreraco-ui <leonardo.herrera.co@cloudappi.net>
2026-02-06 18:04:55 -0500

    fix: realizar peticiones se

--- src/pages/public/Catalogs/index.jsx
+++ src/pages/public/Catalogs/index.jsx
@@ -18,7 +18,7 @@ import { useTranslation } from 'react-i18next';
 
 function Catalog() {
   const { t } = useTranslation();
-  const { catalogs, filtersCatalogs, backUpCatalogs, loadingCatalogs} = useSelector((state) => state.catalogs);
+  const { catalogs, backUpCatalogs, loadingCatalogs} = useSelector((state) => state.catalogs);
   const [filtersSelect, setFiltersSelect] = useState([]);
   const [searchApiInputValue, setSearchApiInputValue] = useState('');
   const dispatch = useDispatch();
@@ -32,11 +32,11 @@ function Catalog() {
   }, [apiPage, dispatch]);
 
   useEffect(() => {
-    if (!hasFetched.current && catalogs?.length === 0 && Object.keys(filtersCatalogs).length === 0) {
+    if (!hasFetched.current && catalogs?.length === 0) {
       dispatch(getcatalogs());
       hasFetched.current = true;
     }
-  }, [catalogs, filtersCatalogs, dispatch]);
+  }, [dispatch, catalogs]);
 
   const filterApiBanner = apiPage && apiPage.contentSections && apiPage.contentSections?.length > 0 ? apiPage.contentSections.filter((item) => item.__component === 'home.banner-section') : [];
 
