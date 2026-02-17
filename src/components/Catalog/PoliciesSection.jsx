654a4f9223e84056320e404665990afc10bc3693
leonardoherreraco-ui <leonardo.herrera.co@cloudappi.net>
2026-02-16 19:16:42 -0500

    feat: view de policies

--- src/components/Catalog/PoliciesSection.jsx
+++ src/components/Catalog/PoliciesSection.jsx
@@ -1,33 +1,170 @@
 import React from "react";
 import classes from "./section.module.scss";
 import { useTranslation } from 'react-i18next';
+import MoreVertIcon from "@mui/icons-material/MoreVert";
+import CheckCircleIcon from '@mui/icons-material/CheckCircle';
+import LocationOnOutlinedIcon from '@mui/icons-material/LocationOnOutlined';
+import CancelRoundedIcon from '@mui/icons-material/CancelRounded';
 
 function SectionPolicies({ policies }) {
   const { t } = useTranslation();
 
-  const usage = policies?.credentialSubject?.["gx:usagePolicy"] || {};
-  const permissions = usage["odrl:permission"] || [];
-  const prohibitions = usage["odrl:prohibition"] || [];
+  const rootStyles = getComputedStyle(document.documentElement);
+  const primaryColor = rootStyles.getPropertyValue('--primary-color').trim();
 
+  const titleIconAccessPolicies = {
+    "gx:compliance": {
+      "title": "Gaia-X Compliance",
+      "label": "Compliance Level",
+      "icon": <CheckCircleIcon sx={{ color: primaryColor }} />
+    },
+    "gx:location": {
+      "title": "Geographic Restriction",
+      "label": "Location",
+      "icon": <LocationOnOutlinedIcon sx={{ color: primaryColor }} />
+    }
+  }
+
+  const subject = policies?.credentialSubject || {};
+  const generalInfo = {
+    id: subject["id"] || "—",
+    policyName: subject["gx:policyName"] || "—",
+    issuer: policies["issuer"] || "—",
+    issuanceDate: policies["issuanceDate"] || "—",
+    type: policies["type"] || "—",
+  };
+  const accessPolicy = subject["gx:accessPolicy"] || {};
+  const usagePolicy = subject["gx:usagePolicy"] || {};
+  const permissions = usagePolicy["odrl:permission"] || [];
+  const prohibitions = usagePolicy["odrl:prohibition"] || [];
+  const contractDefinitions = subject["gx:contractDefinitions"] || {};
+  
   return (
-    <div className={classes.section}>
-      <h1 className={classes.section_title}>{t("Catalogs.Policy.title")}</h1>
+    <div className={classes.participant_container}>
+      <section className={classes.column}>
+        <h3>General Information</h3>
+        <div className={classes.card}>
+          <div className={classes.info_row}>
+            <label>Polley ID</label>
+            <span className={``}></span>
+          </div>
 
-      <h2 className={classes.policy_group_title}>{t("Catalogs.Policy.permissions")}</h2>
-      {permissions.map((p, i) => (
-        <div className={classes.policy_card + classes.policy_card__perm} key={i}>
-          <div className={classes.policy_header}>{t("Catalogs.Policy.permission")}</div>
-          <p>{t("Catalogs.Policy.allow")}: {p["odrl:action"]}</p>
-        </div>
-      ))}
+          <div className={classes.info_row}>
+            <label>Policy Name</label>
+            <MoreVertIcon style={{ cursor: "pointer" }} />
+          </div>
 
-      <h2 className={classes.policy_group_title}>{t("Catalogs.Policy.prohibitions")}</h2>
-      {prohibitions.map((p, i) => (
-        <div className={classes.policy_card + classes.policy_card__rohib} key={i}>
-          <div className={classes.policy_header}>{t("Catalogs.Policy.prohibition")}</div>
-          <p>{t("Catalogs.Policy.forbiden")}: {p["odrl:action"]}</p>
+          <div className={classes.info_row}>
+            <label>Issuer</label>
+            <MoreVertIcon style={{ cursor: "pointer" }} />
+          </div>
+
+          <div className={classes.info_row}>
+            <label>Issuance Date</label>
+            <MoreVertIcon style={{ cursor: "pointer" }} />
+          </div>
+
+          <div className={classes.info_row}>
+            <label>Policy Type</label>
+            <span className={``}></span>
+          </div>
         </div>
-      ))}
+      </section>
+
+      <section className={classes.column}>
+        <h3>Access Policies</h3>
+        <div className={classes.void_card}>
+          {accessPolicy && accessPolicy['gx:rules'] && accessPolicy['gx:rules'].length > 0 ? (
+            accessPolicy['gx:rules'].map((pol, idx) => (
+              <div className={classes.sub_card} key={idx}>
+                <div className={classes.icon_text}>
+                  <span>{titleIconAccessPolicies[pol['attribute']].icon}</span>
+                  <div>
+                    <strong>{titleIconAccessPolicies[pol['attribute']].title}</strong>
+                    <p>{titleIconAccessPolicies[pol['attribute']].label}: {Array.isArray(pol['value']) ? pol['value'].join(' / ') : pol['value']}</p>
+                  </div>
+                </div>
+              </div>
+            ))
+          ) : (
+            <div className={classes.sub_card}>
+              <div className={classes.icon_text}>
+                <span>🏢</span>
+                <div>
+                  <strong>No access policies</strong>
+                  <p>{t("Catalogs.participant.noneLabel")}</p>
+                </div>
+              </div>
+            </div>
+          )}
+        </div>
+      </section>
+
+      <section className={classes.column}>
+        <h3>Usage Policies & contract</h3>
+        {permissions && permissions.length > 0 ?
+        permissions.map((per, idx) => (
+          <div className={`${classes.void_card} ${classes.highlight_card}`} key={idx}>
+            <div className={classes.icon_text}>
+              <CheckCircleIcon sx={{ color: "white" }} />
+              <div>
+                <strong>{per["odrl:action"].split(':')[1]}</strong>
+                <p>
+                  Usage for veterinary research until
+                  {(per["odrl:constraint"][0]["odrl:rightOperand"].match(/\b\d{4}\b/) || [])[0]}
+                </p>
+              </div>
+            </div>
+          </div>
+        )) :
+          <div className={classes.void_card}>
+            <div className={classes.icon_text}>
+              <div>❌</div>
+              <div>
+                <strong>{t("Catalogs.participant.usage")}</strong>
+                <p>{t("Catalogs.participant.noUsagePolicy")}</p>
+              </div>
+            </div>
+          </div>
+        }
+        {prohibitions && prohibitions.length > 0 ?
+        prohibitions.map((pro, idx) => (
+          <div className={`${classes.void_card} ${classes.highlight_red_card}`} key={idx}>
+            <div className={classes.icon_text}>
+              <CancelRoundedIcon sx={{ color: "white" }} />
+              <div>
+                <strong>{pro["odrl:action"].split(':')[1]}</strong>
+                <p>
+                  {pro["description"]}
+                </p>
+              </div>
+            </div>
+          </div>
+        )) :
+          <div className={classes.void_card}>
+            <div className={classes.icon_text}>
+              <div>❌</div>
+              <div>
+                <strong>{t("Catalogs.participant.usage")}</strong>
+                <p>{t("Catalogs.participant.noUsagePolicy")}</p>
+              </div>
+            </div>
+          </div>
+        }
+        <div className={classes.card}>
+          <div className={classes.icon_text}>
+            <div>⚖️</div>
+            <div>
+              <strong>Legal Template</strong>
+              <p className={classes.details}>Terms & Condition: {contractDefinitions["gx:arbitration"]}</p>
+              <p className={classes.details}>Governing LAW {contractDefinitions["gx:governingLaw"]}</p>
+            </div>
+          </div>
+          <button className={classes.btn_action}>
+            View Full ODRL Policy
+          </button>
+        </div>
+      </section>
     </div>
   );
 }
